7ffe4a1e428c095f4d67cbc5958945c3146236d1
Tyler Witlin <twitlin@witl.xyz>
2023-12-30 11:59:05 -0500

    fix: add password for sudo
    Signed-off-by: Tyler Witlin <twitlin@witl.xyz>

--- nix/modules/common/users/twitlin/nixos.nix
+++ nix/modules/common/users/twitlin/nixos.nix
@@ -12,6 +12,7 @@ in {
       "network"
       "samba-users"
     ];
+    password = "$6$BF1VGgxuvHvcDzid$OUjyjst5IUOruRYyqloSLL7gxXAt8ipi31xO.3jvl9HzKz8p0UBkz32Ex2rUqhF3smpfbJU3Yzws/yxin32tX0";
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPnKXgEGczcu8lGs+DEvRWgI4cSYHkAyTAU6/SMAHjL4 twitlin@mbp16inch2021.3520.dhcp.asu.edu"
